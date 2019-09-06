0c359f5156f1b172dba43afd78612ee0242fa4e1
Piyadon <piyadon@Piyadons-MacBook-Pro.local>
2019-09-06 14:57:43 +0700

    user add regis

--- src/main/java/com/hackathon/cardservice/model/userlogin/UserLogin.java
+++ src/main/java/com/hackathon/cardservice/model/userlogin/UserLogin.java
@@ -1,13 +1,16 @@
 package com.hackathon.cardservice.model.userlogin;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Data
 public class UserLogin {
 
     @NotNull
-    private String username;
+    private String userName;
 
     @NotNull
     private String password;
--- src/main/java/com/hackathon/cardservice/service/UserService.java
+++ src/main/java/com/hackathon/cardservice/service/UserService.java
@@ -1,14 +1,19 @@
 package com.hackathon.cardservice.service;
 
+import com.hackathon.cardservice.constants.Response;
+import com.hackathon.cardservice.exception.CardException;
 import com.hackathon.cardservice.model.Users;
 import com.hackathon.cardservice.model.userlogin.UserLogin;
 import com.hackathon.cardservice.repositories.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
     private static final Logger log = LogManager.getLogger(CardService.class.getName());
     private UserRepository userRepository;
@@ -17,12 +22,15 @@ public class UserService {
         this.userRepository = userRepository;
 
     }
-    public boolean verifyUser(UserLogin userLogin){
-        List<Users> userList= userRepository.getUserFromUsername(userLogin.getUsername());
+    public boolean verifyUser(UserLogin userLogin) throws CardException {
+        List<Users> userList= userRepository.getUserFromUsername(userLogin.getUserName());
         if(userList.isEmpty() || userList.size() >1){
-            return false;
+            throw new CardException(Response.USER_NOTFOUND, HttpStatus.BAD_REQUEST);
         }
         return (userList.get(0).getPassword().equals(userLogin.getPassword())) ? true : false;
+    }
+    public void regisUSer(Users user){
+        userRepository.save(user);
     }
 
 }
