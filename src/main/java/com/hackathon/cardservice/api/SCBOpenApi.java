7cc70006523a322517c61c737c24729113f0a3c7
theAutotelicX <feslta.renerenzxe7@gmail.com>
2019-09-08 01:01:00 +0700

    fix 5000 scbapi

--- src/main/java/com/hackathon/cardservice/api/SCBOpenApi.java
+++ src/main/java/com/hackathon/cardservice/api/SCBOpenApi.java
@@ -6,9 +6,7 @@ import com.hackathon.cardservice.util.Util;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,18 +30,24 @@ public class SCBOpenApi {
         JSONObject keyBody = new JSONObject();
         keyBody.put("applicationKey", "l7d1c5e7b0d8f54828841690126a303d58");
         keyBody.put("applicationSecret", "57a4c9f0a0ad4d7ab0c0373b9b382772");
-        RequestEntity<JSONObject> requestEntity = RequestEntity.post(URI.create(host + "/v1/oauth/token"))
-                .header("Content-Type","application/json")
+        try{
+
+            RequestEntity requestEntity = RequestEntity.post(URI.create(host + "/v1/oauth/token"))
                 .header("resourceOwnerId", "l7d1c5e7b0d8f54828841690126a303d58")
                 .header("requestUId", UUID.randomUUID().toString())
                 .header("accept-language", "EN")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .body(keyBody);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
-        System.err.println(responseEntity);
-        if(responseEntity.getStatusCode().value() == 200){
-            System.err.println(Util.readValue(data, TokenResponseModel.class));
-            return Util.readValue(data, TokenResponseModel.class);
+                .body(keyBody.toString());
+            ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+            System.err.println(responseEntity);
+            if(responseEntity.getStatusCode().value() == 200){
+                System.err.println(Util.readValue(data, TokenResponseModel.class));
+                return Util.readValue(data, TokenResponseModel.class);
+            }
+        }
+        catch (Exception exception){
+            System.err.println("exception: " + exception);
+            throw exception;
         }
         return Util.readValue(data, TokenResponseModel.class);
     }
